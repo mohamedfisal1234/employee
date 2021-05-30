07e70fa1cf672066f5f361f18bfbfdf89f93a42e
mohamedfisal1234 <84065405+mohamedfisal1234@users.noreply.github.com>
2021-05-29 20:02:20 -0700

    Create Employee.java

---
+++ Employee.java
@@ -0,0 +1,23 @@
+public class Employee
+{
+	public static void main(String[] args) {
+	 System.out.print("NAME   ");
+	    System.out.print("YEAR OF JOINING    ");
+	System.out.print("ADRESS");
+			System.out.println()   ;
+
+ System.out.print("robert     ");
+	    System.out.print("1994         ");
+	System.out.print("64c-wallsStreet");
+			System.out.println()   ;
+
+	System.out.print("sam        ");
+	    System.out.print("2000         ");
+	System.out.print("68d-WallsStreet");
+			System.out.println()   ;
+		System.out.print("john       ");
+	    System.out.print("1999         ");
+	System.out.print("26B-WallsStreet");
+			System.out.println()   ;	
+	}
+}
